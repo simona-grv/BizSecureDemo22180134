d5a0c6aaf5bd89f7f55c994ea7e0c370080323ec
sgrigorova_22180134@unwe.bg <sgrigorova_22180134@unwe.bg>
2026-03-11 15:00:44 +0200

    Completed Exercise 5 - SQL Injection and Parameterized Queries

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,35 @@
+﻿using BizSecureDemo22180134.Data;
+using BizSecureDemo22180134.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+namespace BizSecureDemo22180134.Controllers;
+[Authorize]
+public class SearchController : Controller
+{
+    private readonly AppDbContext _db;
+    public SearchController(AppDbContext db)
+    {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Results(string keyword)
+    {
+        // var sql = $"SELECT * FROM Orders WHERE Title LIKE '%{keyword}%'";
+        // var results = await _db.Orders.FromSqlRaw(sql).ToListAsync();
+
+        var sql = "SELECT * FROM Orders WHERE Title LIKE @keyword";
+        var param = new SqlParameter("@keyword", $"%{keyword}%");
+
+        var results = await _db.Orders
+            .FromSqlRaw(sql, param)
+            .ToListAsync();
+
+        return View(results);
+    }
+}
